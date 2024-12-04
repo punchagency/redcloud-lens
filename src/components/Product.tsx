3a1b5f5f41ba8ef8c75fb6525c97939bf399007f
abubakar musa <abubakarbalamusa1@gmail.com>
2024-12-04 15:51:09 +0100

    minor ui update

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,28 +1,29 @@
+import { Stack } from '@mui/material';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
+import { ProductType } from '../types/product';
 
-interface ProductType {
-  ProductName: string;
-  ProductImage: string;
-  SellerName: string;
-}
-
-const Product: React.FC<ProductType> = ({ ProductName, ProductImage, SellerName }) => {
+const Product: React.FC<ProductType> = ({ ProductName, ProductImage, CategoryName, ProductPrice }) => {
   return (
-    <Card sx={{ width: '100%', height: '35vh'}}>
+    <Card sx={{ width: '100%', height: '40vh'}}>
         <CardContent>
             <Box sx={{ display: 'flex', justifyContent: 'center', alignItems:'center' }}>
                 <img src={ProductImage} alt={ProductName} style={{ height: '20vh', maxHeight: '20vh', marginRight: '10px' }} />
                 </Box>
           <Typography gutterBottom variant="h5" component="div">
-          {ProductName?.slice(0, 25)}{ProductName.length > 25 ? '...' : ''}
+          {ProductName?.slice(0, 50)}{ProductName.length > 50 ? '...' : ''}
+          </Typography>
+          <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {CategoryName}
           </Typography>
           <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            {SellerName}
+            ${ProductPrice}
           </Typography>
+          </Stack>
         </CardContent>
     </Card>
   );
